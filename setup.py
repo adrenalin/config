94763f1c4e5c77e3a661af88f5c49fa01ce00007
Arttu Manninen <arttu@kaktus.cc>
2019-10-17 09:55:37 +0300

    Exclude tests from packages, longer description, minimum Python version

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ Configuration class
 """
 import setuptools
 
+description = 'Multi tier configuration utility that leverages YAML, \
+AWS Secrets Manager and environment variables with tree structures'
+
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
@@ -13,14 +16,21 @@ setuptools.setup(
     version='0.0.1',
     author='Arttu Manninen',
     author_email='arttu@kaktus.cc',
-    description='Configuration utility',
+    description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/adrenalin/config',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(
+        '.',
+        exclude=[
+            'tests',
+            'tests.*'
+        ]
+    ),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
-    ]
+    ],
+    python_requires='>=3.6'
 )
